6c1f27f8ca39f1579b9fb8cddf85b0608250b6e2
jianwei <jianwei.chen@microvirt.com>
2019-04-22 13:46:48 +0800

    Add dir ui
    Signed-off-by: jianwei <jianwei.chen@microvirt.com>

--- app/src/main/java/com/andy/androsecurity/ui/MainActivity.java
+++ app/src/main/java/com/andy/androsecurity/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.andy.androsecurity;
+package com.andy.androsecurity.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.andy.androsecurity.R;
 
 public class MainActivity extends AppCompatActivity {
 
